a3fe1b599f99c1841529d4045d5e3460c03e98c4
Amandabrei <abreidenbach@hotmail.com>
2025-05-17 12:33:57 -0300

    loginSenha

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,30 +1,20 @@
-// src/services/authService.ts
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api', // backend base
+  baseURL: 'http://localhost:8080/api',
 });
 
-// Login e armazenamento do token na sessionStorage
 const login = async (login: string, password: string): Promise<boolean> => {
   try {
-    const response = await API.post('/auth/login', {
-      login,
-      password,
-    });
-
+    const response = await API.post('/auth/login', { login, password });
     const { token } = response.data;
-
-    
     sessionStorage.setItem('token', token);
-
     return true;
   } catch (error) {
     console.error('Erro no login:', error);
     return false;
   }
 };
-
 
 const getToken = (): string | null => {
   return sessionStorage.getItem('token');
